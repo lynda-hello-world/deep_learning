15631c98c3c5c86388b9bccdc342a1d25ac2a42c
lynda <hanlin7172003@yahoo.co.jp>
2023-10-22 20:06:09 +0900

    add code for step06

--- step00.py
+++ step00.py
@@ -1,14 +1,17 @@
 import numpy as np
 class Variable:
     def __init__(self,data):
-        self.data=data
+        self.data=data #ndarry
+        self.grad=None #ndarry
 
 class Function:
     #object:get data from Variable
     #       put calculation result into Variable
     #input:Variable
     #output:Variable
+    #instance variables:input
     def __call__(self,input):
+        self.input=input
         x=input.data #get data from Variable
         y=self.forward(x)
         output=Variable(y) # put calculation result into Variable
@@ -19,6 +22,12 @@ class Function:
     def forward(self,x):
         raise NotImplementedError
     
+    #object:
+    #input:Variable.grad
+    #output:Variable.grad
+    def backward(self,gy):
+        raise NotImplementedError
+    
 class Square(Function):
     #object:calculation
     #input:Variable.data
@@ -26,6 +35,13 @@ class Square(Function):
     def forward(self,x):
         y=x**2
         return y
+    #object:
+    #input:Variable.grad
+    #output:Variable.grad
+    def backward(self, gy):
+        x=self.input.data
+        gx=2*x*gy
+        return gx
 
 class Exp(Function):
     #input:Variable.data
@@ -33,6 +49,12 @@ class Exp(Function):
     def forward(self, x):
         y=np.exp(x)
         return y
+    #input:Variable.grad
+    #output:Variable.grad
+    def backward(self, gy):
+        x=self.input.data
+        gx=np.exp(x)*gy
+        return gx
 
 #input:Function,Variable,eps=1e-4
 #output:number
@@ -44,12 +66,18 @@ def numerical_diff(f,x,eps=1e-4):
     dy=(y1.data-y0.data)/(2*eps)
     return dy
 
-def f(x):
-    A=Square()
-    B=Exp()
-    C=Square()
-    return C(B(A(x)))
+A=Square()
+B=Exp()
+C=Square()
 
 x=Variable(np.array(0.5))
-dy=numerical_diff(f,x)
-print(dy)
+a=A(x)
+b=B(a)
+y=C(b)
+
+y.grad=np.array(1.0)
+b.grad=C.backward(y.grad)
+a.grad=B.backward(b.grad)
+x.grad=A.backward(a.grad)
+
+print(x.grad)
