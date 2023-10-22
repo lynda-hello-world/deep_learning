57a58575c3465b16fcb986a2c629790faf1328cd
lynda <hanlin7172003@yahoo.co.jp>
2023-10-22 17:57:18 +0900

    add code for step02

--- step00.py
+++ step00.py
@@ -1,8 +1,34 @@
+from ast import FunctionDef
 import numpy as np
 class Variable:
     def __init__(self,data):
         self.data=data
 
-data=np.array(1.0)
-x=Variable(data)
-print(x.data)
+class Function:
+    #object:get data from Variable
+    #       put calculation result into Variable
+    #input:Variable
+    #output:Variable
+    def __call__(self,input):
+        x=input.data #get data from Variable
+        y=self.forward(x)
+        output=Variable(y) # put calculation result into Variable
+        return output
+    #object:calculation
+    #input:Variable.data
+    #output:Variable.data
+    def forward(self,x):
+        raise NotImplementedError
+    
+class Square(Function):
+    #object:calculation
+    #input:Variable.data
+    #output:Variable.data
+    def forward(self,x):
+        y=x**2
+        return y
+
+x=Variable(np.array(10))
+f=Square()
+y=f(x)
+print(y.data)
